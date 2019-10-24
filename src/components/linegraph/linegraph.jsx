4115da5fa6e708d726c87f86dfa5a7018df2de5f
James Runnalls <james.runnalls@eawag.ch>
2019-10-24 17:03:46 +0200

    line graph

--- src/components/linegraph/linegraph.jsx
+++ src/components/linegraph/linegraph.jsx
@@ -1,82 +1,76 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import datas from './data.json';
 import './linegraph.css'
 
 class D3LineGraph extends Component {
 
     componentDidMount() {
-        var margin = {top: 50, right: 50, bottom: 50, left: 50}
-  , width = window.innerWidth - margin.left - margin.right // Use the window's width 
-  , height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
+        // Set graph size
+        var margin = {top: 20, right: 20, bottom: 50, left: 50}
+        , width = d3.select("#vis").node().getBoundingClientRect().width - margin.left - margin.right // Use the window's width 
+        , height = d3.select("#vis").node().getBoundingClientRect().height - margin.top - margin.bottom; // Use the window's height
+      
+        var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S");
 
-// The number of datapoints
-var n = 21;
+        // Set the ranges
+        var x = d3.scaleTime().range([0, width]);
+        var y = d3.scaleLinear().range([height, 0]);
 
-// 5. X scale will use the index of our data
-var xScale = d3.scaleLinear()
-    .domain([0, n-1]) // input
-    .range([0, width]); // output
+        // Define the line
+        var valueline = d3.line()
+            .x(function(d) { return x(d.time); })
+            .y(function(d) { return y(d.value); });
 
-// 6. Y scale will use the randomly generate number 
-var yScale = d3.scaleLinear()
-    .domain([0, 1]) // input 
-    .range([height, 0]); // output 
+        // Define the axes
+        var xAxis = d3.axisBottom(x).ticks(5);;
+        var yAxis = d3.axisLeft(y).ticks(5);
 
-// 7. d3's line generator
-var line = d3.line()
-    .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-    .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
-    .curve(d3.curveMonotoneX) // apply smoothing to the line
+        // Adds the svg canvas
+        var svg = d3.select("#vis")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", 
+                "translate(" + margin.left + "," + margin.top + ")");
 
-// 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-var dataset = d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
+        // Set domain
+        x.domain(d3.extent(datas, function(d) {
+            if (typeof(d.time) == "string"){d.time = parseDate(d.time);};
+            return d.time; 
+        }));
+        y.domain(d3.extent(datas, function(d) {
+            d.value = parseFloat(d.value);
+            return d.value; 
+        }));
 
-// 1. Add the SVG to the page and employ #2
-var svg = d3.select("#vis").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        // Add the X Axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
 
-// 3. Call the x axis in a group tag
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+        // Add the Y Axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
 
-// 4. Call the y axis in a group tag
-svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
 
-// 9. Append the path, bind the data, and call the line generator 
-svg.append("path")
-    .datum(dataset) // 10. Binds data to the line 
-    .attr("class", "line") // Assign a class for styling 
-    .attr("d", line); // 11. Calls the line generator 
-
-// 12. Appends a circle for each datapoint 
-svg.selectAll(".dot")
-    .data(dataset)
-  .enter().append("circle") // Uses the enter().append() method
-    .attr("class", "dot") // Assign a class for styling
-    .attr("cx", function(d, i) { return xScale(i) })
-    .attr("cy", function(d) { return yScale(d.y) })
-    .attr("r", 5)
-      .on("mouseover", function(a, b, c) { 
-  			console.log(a) 
-        this.attr('class', 'focus')
-		})
-      .on("mouseout", function() {  })
-      }
-
-  render() {
+        svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(datas));
+       
+        
+    }
+  
+    render() {
         return(
             <div id="vis">
 
             </div>
-          );
-  }
+        );
+    }
 }
 
 export default D3LineGraph;
