54ed47da5738372829496c09ffcc2531d0317aae
James Runnalls <james.runnalls@eawag.ch>
2020-03-25 15:34:45 +0100

    Visual improvements

--- src/components/gis/gis.jsx
+++ src/components/gis/gis.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
-//import { Link } from "react-router-dom";
 import GISMap from "../../graphs/leaflet/gis_map";
-import SidebarLayout from "../../format/sidebarlayout/sidebarlayout";
 import axios from "axios";
 import { apiUrl } from "../../../config.json";
 import FilterBox from "../../components/filterbox/filterbox";
-import "./gis.css";
 import MapLayers from "../../components/maplayers/maplayers";
 import AddLayers from "../../components/addlayers/addlayers";
 import Legend from "../../components/legend/legend";
+import "./gis.css";
 
 class SidebarGIS extends Component {
   render() {
--- src/pages/live/live.jsx
+++ src/pages/live/live.jsx
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 //import { Link } from "react-router-dom";
-import GISMap from "../../graphs/leaflet/gis_map";
-import SidebarLayout from "../../format/sidebarlayout/sidebarlayout";
-import axios from "axios";
-import { apiUrl } from "../../../config.json";
-import FilterBox from "../../components/filterbox/filterbox";
-import "./live.css";
-import MapLayers from "../../components/maplayers/maplayers";
-import AddLayers from "../../components/addlayers/addlayers";
-import Legend from "../../components/legend/legend";
 import GIS from "../../components/gis/gis";
 
 class Live extends Component {
-  render() { 
-    return ( 
-      <GIS title="Live Conditions" documentTitle="Live - Datalakes" selected={[0,1]} />
-     );
+  render() {
+    var colors = [
+      { color: "#000080", point: 0 },
+      { color: "#3366FF", point: 0.142857142857143 },
+      { color: "#00B0DC", point: 0.285714285714286 },
+      { color: "#009933", point: 0.428571428571429 },
+      { color: "#FFFF5B", point: 0.571428571428571 },
+      { color: "#E63300", point: 0.714285714285714 },
+      { color: "#CC0000", point: 0.857142857142857 },
+      { color: "#800000", point: 1 }
+    ];
+    return (
+      <GIS
+        title="Live Conditions"
+        documentTitle="Live - Datalakes"
+        selected={[0, 1]}
+        colors={colors}
+      />
+    );
   }
 }
- 
+
 export default Live;
