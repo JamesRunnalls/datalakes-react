e8221ad91c590716c90e3144e4e277623c067ac3
James Runnalls <james.runnalls@eawag.ch>
2020-03-31 15:46:58 +0200

    Twitter quick link imporved

--- src/components/twitter/twitter.jsx
+++ src/components/twitter/twitter.jsx
@@ -1,49 +1,45 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { format } from "date-fns";
-import { apiUrl } from '../../../src/config.json';
-import twitterImg from './img/twitter.svg';
+import { apiUrl } from "../../../src/config.json";
+import twitterImg from "./img/twitter.svg";
 import "./twitter.css";
 
 class Twitter extends Component {
-    state = {
-        twitterDate: "",
-        twitterText: "",
-        error: false
-        
-    }
-    async componentDidMount(){
-        const { data: response } = await axios.get(apiUrl+'/twitter').catch(error => {
-            this.setState({ error: true});
-          });;
-        var twitterText = response[0].text;
-        var twitterDate = format(new Date(response[0].created_at), "H:mm MMM yy");
-        this.setState({ twitterDate, twitterText });
-    } 
+  state = {
+    link: "",
+    tweet: "",
+    date: "",
+    error: false
+  };
+  async componentDidMount() {
+    const { data: response } = await axios
+      .get(apiUrl + "/twitter")
+      .catch(error => {
+        this.setState({ error: true });
+      });
+
+    const text = response[0].text.split(" ");
+    const link = text.pop();
+    const tweet = text.join(" ");
+    const date = format(new Date(response[0].created_at), "H:mm MMM yy");
+    this.setState({ link, tweet, date });
+  }
 
   render() {
-    const { twitterText, twitterDate, error } = this.state;
-    var twitter = "twitter"
-    if (error){twitter = "twitter hide"};
-    var twitterTextLink = [];
-    var id = 0;
-    for (var work of twitterText.split(" ")){
-        id += 1;
-        if (work.includes("http")){
-            twitterTextLink.push(<React.Fragment key={id}><a href={work}>here</a></React.Fragment>);
-        } else {
-            twitterTextLink.push(<React.Fragment key={id}>{work}</React.Fragment>);
-        }
-        id += 1;
-        twitterTextLink.push(<React.Fragment key={id}> </React.Fragment>);
-    }
-    twitterTextLink = twitterTextLink.slice(0, -1);
-    return (
-          <div className={twitter}>
-              <a href="https://twitter.com/Datalakes1"><img src={twitterImg} alt="Twitter" /></a>
-              <div className="twitter-text">"{twitterTextLink}"</div>
-              <div className="twitter-date">Tweeted {twitterDate}</div>
-          </div>
+    const { link, tweet, date, error } = this.state;
+    return !error ? (
+      <div className="twitter" title="View tweet on twitter">
+        <a href="https://twitter.com/Datalakes1">
+          <img src={twitterImg} alt="Twitter" />
+        </a>
+        <a href={link}>
+          <div className="twitter-text">"{tweet}"</div>
+          <div className="twitter-date">Tweeted {date}</div>
+        </a>
+      </div>
+    ) : (
+      <div></div>
     );
   }
 }
