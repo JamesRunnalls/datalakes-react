9b8db93ebc2b4b879d0fa8760251e39b4feeec70
James Runnalls <james.runnalls@eawag.ch>
2020-07-23 12:11:44 +0200

    Add api documentation

--- src/pages/api/api.jsx
+++ src/pages/api/api.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { apiUrl } from "../../config.json";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
 import "./api.css";
 
 class API extends Component {
@@ -6,15 +9,13 @@ class API extends Component {
     document.title = "API - Datalakes";
     return (
       <React.Fragment>
+        <h1>Datalakes API</h1>
+        <div className="doc-desc">
+          Documentation for the Datalakes API is generated automatically using{" "}
+          <a href="https://swagger.io/" target="_blank" rel="noopener noreferrer">Swagger.</a>
+        </div>
         <div className="api-container">
-          <h1>API Documentation</h1>
-          <div className="api-inner">
-            <h3>Datalakes is still under active developement.</h3>
-            Datalakes will provide direct access to data via its REST API.
-            However as the API is constantly changing as the project progresses
-            and grows, full documentation of the API is being delayed until the
-            API begins to stabalise.
-          </div>
+          <SwaggerUI url={apiUrl + "/docs"} />
         </div>
       </React.Fragment>
     );
