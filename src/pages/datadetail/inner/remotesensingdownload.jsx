a6e6d685bfa37a117a0ec0a995442400fe563213
James Runnalls <james.runnalls@eawag.ch>
2020-11-23 10:26:23 +0100

    Calendar updates

--- src/pages/datadetail/inner/remotesensingdownload.jsx
+++ src/pages/datadetail/inner/remotesensingdownload.jsx
@@ -1,45 +1,95 @@
 import React, { Component } from "react";
 import "swagger-ui-react/swagger-ui.css";
 import SwaggerUI from "swagger-ui-react";
+import Calendar from "react-calendar";
 import "../datadetail.css";
 
 class RemoteSensingDownload extends Component {
   state = {
-    download: 0,
+    start: new Date(),
+    end: new Date(),
+    selected_files: [],
   };
 
-  onChangeDownload = (event) => {
-    this.setState({
-      download: event.target.value,
+  onChangeRange = (value) => {
+    var { files } = this.props;
+    var start = value[0];
+    var end = value[1];
+    var selected_files = this.selectedFiles(files, start, end);
+    this.setState({ start, end, selected_files });
+  };
+
+  selectedFiles = (files, start, end) => {
+    var selected_files = files.filter(
+      (f) => f.mindatetime >= start && f.maxdatetime <= end
+    );
+    selected_files = selected_files.map((sf) => {
+      let path = sf.filelink.split("/");
+      let name = path.pop().split(".")[0];
+      let name_path = name.split("_");
+      sf.folder = path.join("/");
+      sf.nc = `${sf.folder}/${name_path[0]}_${name_path[2]}_${name_path[3]}.nc`;
+      sf.satellite = name_path[2];
+      return sf;
     });
+    return selected_files;
   };
 
   async componentDidMount() {
     var { files } = this.props;
-    this.setState({ download: files.length - 1 });
+    files = files.map((f) => {
+      f.maxdatetime = new Date(f.maxdatetime);
+      f.mindatetime = new Date(f.mindatetime);
+      return f;
+    });
+
+    var end = new Date(this.props.dataset.maxdatetime);
+    var start = new Date(end.getTime() - 4 * 24 * 60 * 60 * 1000);
+
+    var selected_files = this.selectedFiles(files, start, end);
+
+    this.setState({
+      start,
+      end,
+      files,
+      selected_files,
+    });
   }
 
   render() {
-    const { getLabel, dataset, files } = this.props;
-    var { download } = this.state;
+    const { getLabel, dataset } = this.props;
+    var { start, end, selected_files } = this.state;
     var { mindatetime, maxdatetime } = dataset;
     mindatetime = new Date(mindatetime);
     maxdatetime = new Date(maxdatetime);
 
-    var git = "https://gitlab.com/eawag-rs/sencast";
-    var link = files[download].filelink;
-    var linknc = files[download].filelink.replace(
-      "remotesensing/",
-      "remotesensing/nc/"
-    );
-    var filelist = [];
-    for (var i = 0; i < files.length; i++) {
-      filelist.push(
-        <option key={files[i].id} value={i}>
-          {new Date(files[i].mindatetime).toString()}
-        </option>
+    var table_inner = [];
+    for (var file of selected_files) {
+      console.log(file);
+      table_inner.push(
+        <tr key={file.filelink}>
+          <td>{file.satellite}</td>
+          <td>
+            {file.mindatetime.toLocaleTimeString() +
+              " " +
+              file.mindatetime.toLocaleDateString()}
+          </td>
+          <td title="Download JSON">
+            <a href={file.filelink}>
+              <button>JSON</button>
+            </a>
+          </td>
+          <td title="Download NetCDF">
+            <a href={file.nc}>
+              <button>NetCDF</button>
+            </a>
+          </td>
+        </tr>
       );
     }
+
+    var git = "https://gitlab.com/eawag-rs/sencast";
+
     var url = "https://api.datalakes-eawag.ch/externaldata/remotesensing/";
     var swagger = url + "api";
 
@@ -67,16 +117,28 @@ class RemoteSensingDownload extends Component {
         </p>
 
         <div className="info-title">Download</div>
-        <div className="meteolakesdownload">
-          <select value={download} onChange={this.onChangeDownload}>
-            {filelist}
-          </select>
-          <a href={link}>
-            <button>Download JSON</button>
-          </a>
-          <a href={linknc}>
-            <button>Download NetCDF</button>
-          </a>
+        <div className="remotesensingdownload">
+          <div className="downloadinner">
+            <Calendar
+              selectRange={true}
+              minDate={mindatetime}
+              maxDate={maxdatetime}
+              value={[start, end]}
+              onChange={this.onChangeRange}
+            />
+          </div>
+          <div className="downloadinner">
+            <table>
+              <thead>
+                <tr>
+                  <th>Satellite</th>
+                  <th>Datetime</th>
+                  <th colSpan="2">Download</th>
+                </tr>
+              </thead>
+              <tbody>{table_inner}</tbody>
+            </table>
+          </div>
         </div>
         <div className="info-title">API</div>
 
