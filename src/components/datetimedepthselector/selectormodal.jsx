cdb04357db3c8d542519bdca2fbe9d76099d6047
JamesRunnalls <james.runnalls@eawag.ch>
2021-05-20 17:58:42 +0200

    Fix calendar

--- src/components/datetimedepthselector/selectormodal.jsx
+++ src/components/datetimedepthselector/selectormodal.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
 import "./datetimedepthselector.css";
 import DepthSelector from "./depthselector";
 
