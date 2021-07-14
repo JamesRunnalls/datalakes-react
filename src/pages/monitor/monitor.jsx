aa1a5db32792cbab53668508f719e62ea70cee44
JamesRunnalls <james.runnalls@eawag.ch>
2021-07-14 13:32:47 +0200

    Add debugging guide

--- src/pages/monitor/monitor.jsx
+++ src/pages/monitor/monitor.jsx
@@ -31,7 +31,7 @@ class Monitor extends Component {
     var { monitor } = this.state;
     return (
       <div className="">
-        <h1>Status of Live Datasets</h1>
+        <h2>Datalakes Monitoring</h2>
         <table>
           <tbody>
             <tr>
@@ -60,6 +60,46 @@ class Monitor extends Component {
             })}
           </tbody>
         </table>
+        <h1>Debugging</h1>
+        <p>
+          There are a number of sections of the data pipeline that can break
+          down, listed here are common fixes that can help to restart a broken
+          pipeline. The main sections of the pipeline are as follows:
+        </p>
+        <ul>
+          <li>Transfer from intrument</li>
+          <li>Processing of data</li>
+          <li>Upload to renkulab</li>
+          <li>Datalakes update</li>
+        </ul>
+        <p>
+          In order to detect which section of the process is broken the
+          following process can be followed.
+        </p>
+        <ol>
+          <li>
+            Login to renkulab and check when was the last time data was uploaded
+            to the repository, if this is recent then this could be an issue
+            with the connection between Renkulab and Datalakes. This can likely
+            only be fixed by James Runnalls. Otherwise it is likely to be an
+            issue with the data processing so check that the format of recently
+            produced files is consistent with old files.
+          </li>
+          <li>
+            If data has not been uploaded to renkulab recently the next thing to
+            try is to login to the Lexplore machine, navigate to the repository
+            in git bash and check the git status of the repo. Sometime the git
+            push can fail and you will see local is x commits ahead of remote.
+            Here the git push command should fix the pipeline.
+          </li>
+          <li>
+            If there are no commits then it means that either the processing
+            step has failed or the instrument has stopped sending data (or is
+            sending data to a new location). Look to see if new data has arrived
+            in the expected folders, if not then its likely a problem with the
+            instrument.
+          </li>
+        </ol>
       </div>
     );
   }
